07c5bf7230d13d3da5a18e688c5f5f4d78adbac9
PeteDWard <45257631+PeteDWard@users.noreply.github.com>
2018-12-20 13:25:11 +0000

    Add files via upload

---
+++ TheGildedRose/InventoriesFactory.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheGildedRose
+{
+    class InventoriesFactory
+    {
+        public static InventoriesInterface Create(Program.Item item, int IndexValue)
+        {
+            if (item.Name == "Aged Brie")
+            {
+                return new UpdateAgedBrie();
+            }
+            else if (item.Name == "Backstage passes" && IndexValue == 7)
+            {
+                return new UpdateBackstagePass();
+            }
+            else if (item.Name == "Backstage passes" && IndexValue == 6)
+            {
+                return new UpdateBackstagePass();
+            }
+            else if (item.Name == "Normal Item" && IndexValue == 4)
+            {
+                return new UpdateNormalItem();
+            }
+            else if (item.Name == "Normal Item" && IndexValue == 3)
+            {
+                return new UpdateNormalItem();
+            }
+            else if (item.Name == "Conjured" && IndexValue == 1)
+            {
+                return new UpdateConjuredItem();
+            }
+            else if (item.Name == "Conjured" && IndexValue == 0)
+            {
+                return new UpdateConjuredItem();
+            }
+            else if (item.Name == "Sulfuras")
+            {
+                return new DoNothing();
+            }
+            else
+            {
+                return new InvalidItems();
+            }
+        }
+    }
+
+    interface InventoriesInterface
+    {
+        void Update(Program.Item item);
+    }
+
+    #region Inventory Update
+    class UpdateAgedBrie : InventoriesInterface
+    {
+        public void Update(Program.Item item)
+        {
+            if (item.Quality < item.QualityMax)
+            {
+                item.Quality += 1;
+            }
+
+            item.SellIn -= 1;
+
+            if (item.SellIn < 0)
+            {
+                if (item.Quality < item.QualityMax)
+                {
+                    item.Quality += 1;
+                }
+            }
+        }
+    }
+
+    class UpdateBackstagePass : InventoriesInterface
+    {
+        public void Update(Program.Item item)
+        {
+            if (item.SellIn > 0)
+            {
+                if (item.Quality < item.QualityMax)
+                {
+                    item.Quality += 1;
+                }
+            }
+
+            if (item.SellIn < 11)
+            {
+                if (item.Quality < item.QualityMax)
+                {
+                    item.Quality += 1;
+                }
+            }
+
+            if (item.SellIn < 6)
+            {
+                if (item.Quality < item.QualityMax)
+                {
+                    item.Quality += 1;
+                }
+            }
+
+            item.SellIn -= 1;
+
+            if (item.SellIn < 0)
+            {
+                item.Quality = 0;
+            }
+        }
+    }
+
+    class UpdateNormalItem : InventoriesInterface
+    {
+        public void Update(Program.Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality -= 1;
+            }
+            
+            item.SellIn -= 1;
+
+            if (item.SellIn < 0)
+            {
+                if (item.Quality > 0)
+                {
+                    item.Quality -= 1;
+                }
+            }
+
+            if (item.Quality > item.QualityMax)
+            {
+                item.Quality = item.QualityMax;
+            }
+        }
+    }
+
+    class UpdateConjuredItem : InventoriesInterface
+    {
+        public void Update(Program.Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality -= 2;
+            }
+            
+            item.SellIn -= 1;
+
+            if (item.SellIn < 0)
+            {
+                if (item.Quality > 0)
+                {
+                    item.Quality -= 2;
+                }
+            }
+        }
+    }
+
+    class InvalidItems : InventoriesInterface
+    {
+        public void Update(Program.Item item)
+        {
+            item.Name = "NO SUCH ITEM";
+            item.SellIn = 0;
+            item.Quality = 0;
+        }
+    }
+
+    class DoNothing : InventoriesInterface
+    {
+        public void Update(Program.Item item)
+        {
+
+        }
+    }
+    #endregion
+}
---
+++ TheGildedRose/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheGildedRose
+{
+    public class Program
+    {
+        public IList<Item> Items;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Gilded Rose Inventory");
+
+            var app = new Program()
+            {
+                Items = PopulateItems()
+            };
+
+            app.ShowInventory();
+
+            app.UpdateQuality();
+
+            app.ShowInventory();
+
+            Console.ReadKey();
+        }
+
+        public void UpdateQuality()
+        {
+            int ItemQty = Items.Count;
+            foreach (Item ThisItem in Items)
+            {
+                ItemQty -= 1;
+                InventoriesInterface inventory = InventoriesFactory.Create(ThisItem, ItemQty);
+                inventory.Update(ThisItem);
+            }
+        }
+
+        public void ShowInventory()
+        {
+            Console.WriteLine("\n===== Inventory");
+
+            foreach (Item ThisItem in Items)
+            {
+                Console.WriteLine(ThisItem.Name + " " + ThisItem.SellIn + " " + ThisItem.Quality);
+            }
+        }
+
+        public class Item
+        {
+            public string Name { get; set; }
+            public int SellIn { get; set; }
+            public int Quality { get; set; }
+
+            public int QualityMax = 50;
+        }
+
+        public static List<Item> PopulateItems()
+        {
+            List<Item> Items = new List<Item>
+                        {
+                            new Item {Name = "Aged Brie", SellIn = 1, Quality = 1},
+                            new Item {Name = "Backstage passes", SellIn = -1, Quality = 2},
+                            new Item {Name = "Backstage passes", SellIn = 9, Quality = 2},
+                            new Item {Name = "Sulfuras", SellIn = 2, Quality = 2},
+                            new Item {Name = "Normal Item", SellIn = -1, Quality = 55},
+                            new Item {Name = "Normal Item", SellIn = 2, Quality = 2},
+                            new Item {Name = "INVALID ITEM", SellIn = 2, Quality = 2},
+                            new Item {Name = "Conjured", SellIn = 2, Quality = 2},
+                            new Item {Name = "Conjured", SellIn = -1, Quality = 5}
+                        };
+            return Items;
+        }
+    }
+}
